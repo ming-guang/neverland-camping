ccc5a0929199405d89217936c48ba0a85ceeaca7
ming-guang <athoni-contact@relay.akimitsu.dev>
2024-01-09 21:57:02 +0700

    feat: implement courses level description for homepage

--- src/ui/src/views/HomeView.vue
+++ src/ui/src/views/HomeView.vue
@@ -4,6 +4,7 @@ import WelcomeHero from '@/components/WelcomeHero.vue'
 import CoachesList from '@/components/CoachesList.vue'
 import CoachesListTitle from '@/components/CoachesListTitle.vue'
 import CoachesListLoading from '@/components/CoachesListLoading.vue'
+import CoursesLevelDescription from '@/components/CoursesLevelDescription.vue'
 </script>
 
 <script lang="ts">
@@ -18,6 +19,9 @@ export default {
   <main>
     <GlobalNavbar />
     <WelcomeHero />
+    <div class="py-8 lg:py-16">
+      <CoursesLevelDescription />
+    </div>
     <div class="py-8 lg:py-16">
       <CoachesListTitle />
       <Suspense>
