f309bd40c9ff89a0d58b0b4f26621b5ddf1876d4
ming-guang <athoni-contact@relay.akimitsu.dev>
2024-01-10 01:18:53 +0700

    feat: implement tournament pages

---
+++ src/ui/src/views/TournamentsView.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import GlobalNavbar from '@/components/GlobalNavbar.vue'
+import UpcomingTournamentsListTitle from '@/components/TournamentsView/UpcomingTournamentsListTitle.vue'
+import UpcomingTournamentsList from '@/components/TournamentsView/UpcomingTournamentsList.vue'
+</script>
+
+<script lang="ts">
+export default {
+  metaInfo: {
+    title: 'Tournaments'
+  }
+}
+</script>
+
+<template>
+  <main>
+    <GlobalNavbar />
+    <div class="py-8 lg:py-16">
+      <UpcomingTournamentsListTitle />
+      <Suspense>
+        <UpcomingTournamentsList />
+      </Suspense>
+    </div>
+  </main>
+</template>
