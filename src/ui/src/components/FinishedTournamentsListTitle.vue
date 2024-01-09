1f083631d7a7aa94ea314d9660c8de24119f614a
ming-guang <athoni-contact@relay.akimitsu.dev>
2024-01-09 22:30:03 +0700

    feat: implement finished tournaments list component for home page

---
+++ src/ui/src/components/FinishedTournamentsListTitle.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="grid max-w-screen-xl px-4 mx-auto lg:gap-8 xl:gap-0">
+    <div class="max-w-screen-md mb-8 lg:mb-16 place-self-center text-center">
+      <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+        Tournament
+      </h2>
+      <p class="text-gray-500 sm:text-xl dark:text-gray-400">
+        We eprovide any weekly, monthly, and yearly tournaments here, along with all the background
+        information. Feel free to choose one to participate in if it matches your strength.
+      </p>
+    </div>
+  </div>
+</template>
