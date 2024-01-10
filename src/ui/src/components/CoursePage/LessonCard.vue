28146591af0379fdefc3705a424dbabe6c6bea06
ming-guang <athoni-contact@relay.akimitsu.dev>
2024-01-10 20:30:40 +0700

    feat: implement lessons page

---
+++ src/ui/src/components/CoursePage/LessonCard.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { LessonsRecord } from '@/utils/PocketbaseTypes';
+import { ref } from 'vue';
+const courseUrl = ref<string>('/courses');
+const props = defineProps<{
+    lesson: LessonsRecord,
+}>()
+</script>
+
+<template>
+    <div
+        class="flex flex-col items-center justify-start bg-white border border-gray-200 rounded-lg shadow md:flex-row hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+        <div class="flex flex-col justify-between p-4 leading-normal">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ props.lesson.title }}</h5>
+            <div v-html="props.lesson.description">
+
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/ui/src/views/CourseView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import GlobalNavbar from '@/components/GlobalNavbar.vue'
+import LessonsListTitle from '@/components/CoursePage/LessonsListTitle.vue'
+import LessonsList from '@/components/CoursePage/LessonsList.vue';
+</script>
+<script lang="ts">
+export default {
+  metaInfo: {
+    title: 'Lessons'
+  }
+}
+</script>
+<template>
+  <main>
+    <GlobalNavbar />
+    <div class="py-8 lg:py-16">
+      <LessonsListTitle />
+      <Suspense>
+        <LessonsList />
+      </Suspense>
+    </div>
+  </main>
+</template>
